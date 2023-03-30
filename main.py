24fadf3ec3ac93067fe5f6e733a4d28c3c5a8a09
Ali Razmjoo <ali.razmjoo@owasp.org>
2023-03-30 16:48:03 +0200

    bug fixed

--- main.py
+++ main.py
@@ -162,15 +162,16 @@ def run_codeql_scan(vulnerable, language, codeql_language, address):
     # run codeql scan on repositories
     if vulnerable:
         # run codeql scan on vulnerable repositories
-        directory = "repositories/vulnerable/" + language + "/" + address.split("/")[-1]
-        project_directory = current_directory + "/" + directory
-        report_dir = "scan_results/codeql_scan/vulnerable/" + language + "/" + address.split("/")[-1] 
+        repo_directory = "repositories/vulnerable/" + language + "/" + address.split("/")[-1]
+        project_directory = "/src/" + repo_directory
+        report_dir = "/src/scan_results/codeql_scan/vulnerable/" + language + "/" + address.split("/")[-1] 
     else:
         # run codeql scan on non-vulnerable repositories
-        directory = "repositories/non-vulnerable/" + language + "/" + address.split("/")[-1]
-        project_directory = current_directory + "/" + directory
-        report_dir = "scan_results/codeql_scan/non-vulnerable/" + language + "/" + address.split("/")[-1]
-    os.system(f"docker run --rm -it --privileged -v {project_directory}:/src:Z --entrypoint /bin/bash mcr.microsoft.com/cstsectools/codeql-container -c \"codeql database create --language={codeql_language} --threads=0 --source-root /src /tmp/database --overwrite && cd /src && mkdir -p {report_dir} && codeql database analyze /tmp/database --threads=0 --format csv -o {report_dir}/report.csv\"")
+        repo_directory = "repositories/non-vulnerable/" + language + "/" + address.split("/")[-1]
+        project_directory = "/src/" + repo_directory
+        report_dir = "/src/scan_results/codeql_scan/non-vulnerable/" + language + "/" + address.split("/")[-1]
+    
+    os.system(f"docker run --rm -it --privileged -v {current_directory}:/src:Z --entrypoint /bin/bash mcr.microsoft.com/cstsectools/codeql-container -c \"mkdir -p {report_dir} && cd {project_directory} && codeql database create --language={codeql_language} --threads=0 /tmp/database --overwrite  && codeql database analyze /tmp/database --threads=0 --format csv -o {report_dir}/report.csv\"")
     
     
 
